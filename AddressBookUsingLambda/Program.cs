4b43767506b05e5ec909d6183ebdc17546e83c09
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-31 11:17:04 +0530

    feat : sorting Entries in Alphabetically

--- AddressBookUsingLambda/Program.cs
+++ AddressBookUsingLambda/Program.cs
@@ -14,7 +14,7 @@ namespace AddressBookUsingLambda
             {
 
                 Console.WriteLine("*********************************************************");
-                Console.WriteLine("1.Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts list\n4.Delete members Contacts list\n5.Search for details\n6.View details of city or state\n7.Count by city or state\n8.Exit");
+                Console.WriteLine("1.Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts list\n4.Delete members Contacts list\n5.Search for details\n6.View details of city or state\n7.Count by city or state\n8.Sort the list\n9.Exit");
                 Console.WriteLine("Enter an option:");
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
@@ -38,6 +38,9 @@ namespace AddressBookUsingLambda
                         break;
                     case 7:
                         abd.CountByStateOrCity();
+                        break;
+                    case 8:
+                        abd.SortList();
                         break;
                     default:
                         // to exit from main method
