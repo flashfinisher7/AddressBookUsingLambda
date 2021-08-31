81cdae515d1048cc3472cfb9fed616d1b123453c
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-31 10:51:47 +0530

    Add project files.

---
+++ AddressBookUsingLambda/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AddressBookUsingLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t This is a Address book using Lambda Problem");
+            AddressBookDetails abd = new AddressBookDetails();
+
+            while (true)
+            {
+
+                Console.WriteLine("*********************************************************");
+                Console.WriteLine("1.Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts list\n4.Delete members Contacts list\n5.Exit");
+                Console.WriteLine("Enter an option:");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        abd.AddMember();
+                        break;
+                    case 2:
+                        abd.ViewContacts();
+                        break;
+                    case 3:
+                        abd.EditDetails();
+                        break;
+                    case 4:
+                        abd.DeleteDetails();
+                        break;
+                    case 5:
+                        // to exit from main method
+                        Console.WriteLine("Exited");
+                        return;
+
+                }
+            }
+        }
+    }
+}
